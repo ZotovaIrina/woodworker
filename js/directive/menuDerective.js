641ff3362a9ff883f23ffa50c3d066bc41cefaef
irina <zotovaiv87@gmail.com>
2016-07-10 15:49:44 +0300

    Removed menu

--- js/directive/menuDerective.js
+++
@@ -1,9 +0,0 @@
-var app = require("../app");
-
-app.directive('menuDerective', function () {
-    return {
-        templateUrl: 'template/menu.html',
-        controller: 'menu',
-        css: 'menu.css'
-    };
-});
