c8cafc4eca93a20593a4c4de0b8d229c565e1558
irina <zotovaiv87@gmail.com>
2016-08-04 20:06:42 +0300

    set index.html for all request

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var express = require('express');
+var express = require('express'),
+    path = require('path');
+
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/*', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+
 
 var server = app.listen(4000, function () {
     var host = server.address().address;
